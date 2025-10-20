abf77fedc59cfe318463040b18df9ad025ba2692
Stivenvera <stivenvera300@gmail.com>
2025-10-20 15:29:44 -0500

    Add files via upload

--- 1-esenari interactivo/app.js
+++ 1-esenari interactivo/app.js
@@ -1,10 +1,3 @@
-const body = document.querySelector('body')
-const button = document.querySelector('button')
 
-if( header && button){
-    button.addEventListener('click', function() {
-      header.style.bragroundColor ='rgba(20, 20, 16, 1)';
-    } ) ;
-
-
-}
+const diamantes = document.querySelectorAll(".diamond");
+const contador = document.querySelector(".contador");
