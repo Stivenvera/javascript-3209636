9748cea8629744b88f7b9523c7ac32c5b515b93a
Stivenvera <stivenvera300@gmail.com>
2025-10-21 15:24:03 -0500

    Add files via upload

--- 1-esenari interactivo/app.js
+++ 1-esenari interactivo/app.js
@@ -1,3 +1,25 @@
 
 const diamantes = document.querySelectorAll(".diamond");
 const contador = document.querySelector(".contador");
+let numero= 0
+
+console.log(diamantes)
+console.log(contador)
+
+// crear la funciones
+diamantes.forEach(item=>
+{ /* console.log("elemento:" , item)
+    item.style.filter = "grayscale(1)" */
+
+
+    item.addEventListener("click", () =>{
+        item.classList.add("saltar")
+        numero++
+contador.textContent = numero
+    })
+
+
+item.addEventListener("animationed" ,() =>{
+item.style.display="none"
+}, {once:true})
+})
